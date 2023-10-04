2ea1fd3f819214913b4846dcc9a4674b512553ce
StefNunes <2022255@student.cct.ie>
2023-10-04 13:14:43 +0100

    new int

---
+++ src/Shapes/Triangle.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Shapes;
+
+/**
+ *
+ * @author Stefani Nunes
+ */
+public class Triangle {
+    int side1;
+    int side2;
+    int side3;
+    
+    
+}
