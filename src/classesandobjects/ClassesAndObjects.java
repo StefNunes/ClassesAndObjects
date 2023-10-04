dadc7f79ac6ed81498594ac904156f35bea54736
StefNunes <2022255@student.cct.ie>
2023-10-04 12:49:28 +0100

    wrong way
    the wrong way or long way of doing all the sides of the triangle

--- src/classesandobjects/ClassesAndObjects.java
+++ src/classesandobjects/ClassesAndObjects.java
@@ -14,7 +14,29 @@ public class ClassesAndObjects {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+      Triangle tri1 = new Triangle();
+      Triangle tri2 = new Triangle();
+      Triangle tri3 = new Triangle();
+      Triangle tri4 = new Triangle();
+      Triangle tri5 = new Triangle();
+      Triangle tri6 = new Triangle();
+      
+      int[] sideLenghts = new int[18];
+      //the lenghts of tri1's sides will be first,then the lenghts of tri2's sides etc
+      sideLenghts[0] = 5;
+      sideLenghts[1] = 8;
+      sideLenghts[2] = 9;
+      
+      //tri5 (4, 7, 2)
+      sideLenghts[12] = 5;
+      sideLenghts[13] = 8;
+      sideLenghts[14]= 9;
+         
+      
+      
+      
+      
     }
     
 }
